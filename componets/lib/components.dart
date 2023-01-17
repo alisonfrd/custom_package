04d44696560238d94049f46b49c1c3035df38ff2
alisonfrd <alisonfrd@outlook.com>
2023-01-17 08:46:35 -0400

    aa

--- componets/lib/components.dart
+++ componets/lib/components.dart
@@ -6,9 +6,9 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: 100,
-      color: Colors.black,
+      height: 200,
+      width: 200,
+      color: Colors.red,
     );
   }
 }
