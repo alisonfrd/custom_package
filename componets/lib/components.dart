b1673daa0082e47911cd38f632998a8b63fc55a0
alisonfrd <alisonfrd@outlook.com>
2023-01-17 08:18:48 -0400

    alter1

--- componets/lib/components.dart
+++ componets/lib/components.dart
@@ -6,8 +6,8 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
-      width: 500,
+      height: 200,
+      width: 200,
       color: Colors.cyan,
     );
   }
